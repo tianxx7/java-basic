0cf9ea036a72932d06a66dcb23b5c48216c8f8b7
tianxx7 <1729549607@qq.com>
2020-02-10 21:28:57 +0800

    zookeeper作为注册中心与dubbo整合使用

---
+++ src/main/java/dubbo/zookeeper/Server/DubboServer.java
@@ -0,0 +1,29 @@
+package dubbo.zookeeper.Server;
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
+import dubbo.common.UserService;
+import dubbo.common.UserServiceImpl;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author labvi
+ * @version 1.0.0
+ */
+public class DubboServer {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("zookeeper/zookeeper_provider.xml");
+        context.start();
+        ServiceConfig bean = context.getBean(ServiceConfig.class);
+        List<URL> exportedUrls = bean.getExportedUrls();
+        UserServiceImpl userService = context.getBean(UserServiceImpl.class);
+        userService.setPort(exportedUrls.get(0).getPort());
+        System.out.println();
+        System.in.read();
+    }
+}
---
+++ src/main/java/dubbo/zookeeper/client/DubboClient.java
@@ -0,0 +1,31 @@
+package dubbo.zookeeper.client;
+
+import dubbo.common.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
+/**
+ * @author labvi
+ * @version 1.0.0
+ */
+public class DubboClient {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("zookeeper/zookeeper_consumer.xml");
+        context.start();
+        UserService userService = context.getBean(UserService.class);
+        String cmd;
+        while (!"quit".equals(cmd = read())){
+            System.out.println(userService.getName(cmd));
+        }
+    }
+
+    private static String read() throws IOException {
+        LineNumberReader lineNumberReader = new LineNumberReader(
+                new InputStreamReader(System.in));
+        return lineNumberReader.readLine();
+
+    }
+}
